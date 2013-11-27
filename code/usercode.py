ed981c65d05be64cb09d63ce692033ecd057855e
Joe Sanford <joe@cs.tufts.edu>
2013-11-27 00:19:39 +0000

    Added missing files

---
+++ code/usercode.py
@@ -0,0 +1,27 @@
+from message import *
+import time
+import RPi.GPIO as GPIO
+import pyttsx
+
+def run(self, channel, channel2):
+  while True:
+
+    toSend = Message(self.hostname, "magneto", "Message", "go")
+    toSend = Message.encode(toSend)
+    channel2.basic_publish(exchange="", routing_key="Message", body=toSend)
+    time.sleep(.01)
+    toSend = Message(self.hostname, None, "HwCmd", Message.createImage(pin13=1))
+    toSend = Message.encode(toSend)
+    channel.basic_publish(exchange="", routing_key="HwCmd", body=toSend)
+    time.sleep(.01)
+    time.sleep(1)
+    toSend = Message(self.hostname, "magneto", "Message", "stop")
+    toSend = Message.encode(toSend)
+    channel2.basic_publish(exchange="", routing_key="Message", body=toSend)
+    time.sleep(.01)
+    toSend = Message(self.hostname, None, "HwCmd", Message.createImage(pin13=0))
+    toSend = Message.encode(toSend)
+    channel.basic_publish(exchange="", routing_key="HwCmd", body=toSend)
+    time.sleep(.01)
+    time.sleep(1)
+    
