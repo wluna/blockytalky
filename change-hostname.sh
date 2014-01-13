24976b457b80eb35d07e342ab630ba39f75b197d
Joe Sanford <joe@cs.tufts.edu>
2014-01-13 16:09:10 +0000

    Added change hostname script

---
+++ change-hostname.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+echo "BT hostname changer"
+echo "Enter a hostname: "
+read hostname
+sudo echo $hostname > /etc/hostname
+sudo sed -i -e "s/ironman/$hostname/g" /etc/hosts
+sudo echo $hostname > /boot/coder_settings/hostname.txt
+sudo sed -i -e "s/ironman/$hostname/g" /etc/wpa_supplicant.conf
