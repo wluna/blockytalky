f975646412ede8def48359b871efe903148cf230
Joe Sanford <joe@cs.tufts.edu>
2014-01-15 04:16:37 +0000

    Added support for raw and binary touch sensor use

--- backend/comms_module.py
+++ backend/comms_module.py
@@ -117,7 +117,7 @@ if __name__ == "__main__":
     # Set the logging level and start the client.
     logging.basicConfig(format = "%(levelname)s:\t%(message)s",
                         # filename = "cm.log",
-                        level = logging.INFO)
+                        level = logging.ERROR)
     logging.info("Communicator Module (WebSocket client) starting ...")
 
     # DAX WebSocket (remote component)
