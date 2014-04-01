6095a09674db774fa93b6fa5b15711e9ae7abdff
Eliot Alter <Eliot.Alter@gmail.com>
2014-04-01 18:52:00 -0400

    Use A-Z (excluding O's) when generating GUIDs

--- generate_guid.py
+++ generate_guid.py
@@ -3,8 +3,19 @@ import os
 import binascii
 import sys
 
+#Don't use O so that it isn't confused with 0
+numerals35 = "0123456789abcdefghijklmnpqrstuvwxyz"
+numerals36 = "0123456789abcdefghijklmnopqrstuvwxyz" 
+
+base = len(numerals36)
+
+#Yes, this is linear search, but n is very small and this doesn't get called too often
 def valueOfCharacter(c):
-    return int(float.fromhex(c))
+    return numerals36.index(c)
+
+#Source: http://stackoverflow.com/questions/2267362/convert-integer-to-a-string-in-a-given-numeric-base-in-python
+def baseN(num,b, numerals):
+    return ((num == 0) and numerals[0]) or (baseN(num // b, b, numerals).lstrip(numerals[0]) + numerals[num % b])
 
 #http://en.wikipedia.org/wiki/Luhn_algorithm
 def luhn_checksum(card_number):
@@ -15,19 +26,29 @@ def luhn_checksum(card_number):
     even_digits = digits[-2::-2]
     checksum = 0
     checksum += sum(odd_digits)
+
     for d in even_digits:
-        checksum += sum(digits_of(hex(d*2)[2:]))
-    return checksum % 16
+        checksum += sum(digits_of(baseN(d*2, base, numerals36)))
+    return checksum % base
  
 def is_luhn_valid(card_number):
     return luhn_checksum(card_number) == 0
 
 def calculate_luhn(partial_card_number):
-    check_digit = luhn_checksum(partial_card_number + '0')
-    return check_digit if check_digit == 0 else 16 - check_digit
+    check_digit = luhn_checksum(partial_card_number + numerals36[0])
+    return check_digit if check_digit == 0 else base - check_digit
 
-randomBytes = binascii.hexlify(os.urandom(16))
-checkDigit = hex(calculate_luhn(randomBytes))[2:] #Get rid of 0x at the beginning
-randomBytes += checkDigit
-sys.stdout.write(randomBytes.upper())
+def generate_guid():
+    randomBytes = baseN(int(binascii.hexlify(os.urandom(8)), 16), base - 1, numerals35)
+    checkDigit = numerals36[calculate_luhn(randomBytes)]
+    randomBytes += checkDigit
+    #The checkDigit could be a O, so redo the process.
+    if checkDigit == 'o':
+        return generate_guid()
+    else:
+        return randomBytes
+
+if __name__ == '__main__':
+    guid = generate_guid()
+    sys.stdout.write(guid.upper())
 
