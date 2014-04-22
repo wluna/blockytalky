cf0866e16e8c052cfe5a01ebef65d21b90418557
Joe Sanford <joe@cs.tufts.edu>
2014-04-22 16:52:16 +0000

    Upload should now work if start script is used when device is already running (NOTE: may need to fix permissions first); restart script added

--- start.sh
+++ start.sh
@@ -2,37 +2,29 @@
 
 echo Starting BlockyTalky...
 
-if [ `ps -ef | grep us.py | grep -v grep | awk '{print $2}'` ]
-    then
-        kill -9 `ps -ef | grep us.py | grep -v grep | awk '{print $2}'` &>/dev/null
-	echo "User Script killed"
-fi
-
-if [ `ps -ef | grep hardware_daemon.py | grep -v grep | awk '{print $2}'` ]
-    then
-        kill -9 `ps -ef | grep hardware_daemon.py | grep -v grep | awk '{print $2}'` &>/dev/null
-	echo "Hardware Daemon killed"
-fi
-
-if [ `ps -ef | grep blockly_webserver.py | grep -v grep | awk '{print $2}'` ]
-    then
-        kill -9 `ps -ef | grep blockly_webserver.py | grep -v grep | awk '{print $2}'` &>/dev/null
-	echo "WebServer killed"
-fi
-
-if [ `ps -ef | grep comms_module.py | grep -v grep | awk '{print $2}'` ]
-    then
-        kill -9 `ps -ef | grep comms_module.py | grep -v grep | awk '{print $2}'` &>/dev/null
-	echo "Communications Module killed"
-fi
-
 if [ ! -f /etc/BlockyTalkyID ]; then
     python /home/pi/blockytalky/generate_guid.py | sudo tee /etc/BlockyTalkyID > /dev/null
 fi
 
-python /home/pi/blockytalky/backend/blockly_webserver.py &>/dev/null
-python /home/pi/blockytalky/backend/comms_module.py &>/dev/null
-sudo python /home/pi/blockytalky/backend/hardware_daemon.py &>/dev/null
+if [ ! -f /home/pi/blockytalky/code/rawxml.txt ]; then
+    sudo touch /home/pi/blockytalky/code/rawxml.txt
+    sudo chown pi /home/pi/blockytalky/code/rawxml.txt
+    sudo chmod 775 /home/pi/blockytalky/code/rawxml.txt
+    sudo echo '<xml xmlns = "http://www.w3.org/1999/xhtml"></xml>' > /home/pi/blockytalky/code/rawxml.txt
+fi
+
+if [ ! -f /home/pi/blockytalky/code/usercode.py ]; then
+    touch /home/pi/blockytalky/code/usercode.py
+    sudo chown pi /home/pi/blockytalky/code/usercode.py
+    sudo chmod 775 /home/pi/blockytalky/code/usercode.py
+fi
+
+
+    
+
+python /home/pi/blockytalky/backend/blockly_webserver.py &>/home/pi/blockytalky/bw_log
+python /home/pi/blockytalky/backend/comms_module.py &>/home/pi/blockytalky/cm_log
+sudo python /home/pi/blockytalky/backend/hardware_daemon.py &>/home/pi/blockytalky/hd_log
 #python /home/pi/blockytalky/backend/comms_module.py &>/dev/null
 
 echo BlockyTalky running.
