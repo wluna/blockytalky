660dad4660eb45e54bb002fc837e34afd5cbefc2
Isobel Redelmeier <iredelmeier@gmail.com>
2014-04-22 18:12:19 +0000

    More permissions fixes

--- start.sh
+++ start.sh
@@ -11,20 +11,21 @@ if [ ! -f /home/pi/blockytalky/code/rawxml.txt ]; then
     sudo chown pi /home/pi/blockytalky/code/rawxml.txt
 fi
 
-sudo chmod 775 /home/pi/blockytalky/code/rawxml.txt
-sudo echo '<xml xmlns = "http://www.w3.org/1999/xhtml"></xml>' > /home/pi/blockytalky/code/rawxml.txt
-
 if [ ! -f /home/pi/blockytalky/code/usercode.py ]; then
     touch /home/pi/blockytalky/code/usercode.py
 fi
 
+sudo chmod 775 /home/pi/blockytalky/code/rawxml.txt
+sudo echo '<xml xmlns = "http://www.w3.org/1999/xhtml"></xml>' > /home/pi/blockytalky/code/rawxml.txt
+
 sudo chown pi /home/pi/blockytalky/code/usercode.py
 sudo chmod 775 /home/pi/blockytalky/code/usercode.py
 
+sudo chown pi /home/pi/cm.log
+sudo chmod 664 /home/pi/cm.log
 
-python /home/pi/blockytalky/backend/blockly_webserver.py &>/home/pi/blockytalky/bw_log
-python /home/pi/blockytalky/backend/comms_module.py &>/home/pi/blockytalky/cm_log
-sudo python /home/pi/blockytalky/backend/hardware_daemon.py &>/home/pi/blockytalky/hd_log
-#python /home/pi/blockytalky/backend/comms_module.py &>/dev/null
+python /home/pi/blockytalky/backend/blockly_webserver.py &>/dev/null
+python /home/pi/blockytalky/backend/comms_module.py &>/dev/null
+sudo python /home/pi/blockytalky/backend/hardware_daemon.py &>/dev/null
 
 echo BlockyTalky running.
