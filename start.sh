0ea54b449c0b5ef805c5b3cf5428d1b8dd5ef8b2
root <root@mystique.(none)>
2014-04-22 17:39:42 +0000

    Hardware daemon waits to find sensors and encoders (instead of crashing)

--- start.sh
+++ start.sh
@@ -9,18 +9,18 @@ fi
 if [ ! -f /home/pi/blockytalky/code/rawxml.txt ]; then
     sudo touch /home/pi/blockytalky/code/rawxml.txt
     sudo chown pi /home/pi/blockytalky/code/rawxml.txt
-    sudo chmod 775 /home/pi/blockytalky/code/rawxml.txt
-    sudo echo '<xml xmlns = "http://www.w3.org/1999/xhtml"></xml>' > /home/pi/blockytalky/code/rawxml.txt
 fi
+
+sudo chmod 775 /home/pi/blockytalky/code/rawxml.txt
+sudo echo '<xml xmlns = "http://www.w3.org/1999/xhtml"></xml>' > /home/pi/blockytalky/code/rawxml.txt
 
 if [ ! -f /home/pi/blockytalky/code/usercode.py ]; then
     touch /home/pi/blockytalky/code/usercode.py
-    sudo chown pi /home/pi/blockytalky/code/usercode.py
-    sudo chmod 775 /home/pi/blockytalky/code/usercode.py
 fi
 
+sudo chown pi /home/pi/blockytalky/code/usercode.py
+sudo chmod 775 /home/pi/blockytalky/code/usercode.py
 
-    
 
 python /home/pi/blockytalky/backend/blockly_webserver.py &>/home/pi/blockytalky/bw_log
 python /home/pi/blockytalky/backend/comms_module.py &>/home/pi/blockytalky/cm_log
