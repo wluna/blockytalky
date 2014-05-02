a7899663a49f55d54bbb1fca15e59242ec1f762d
Isobel Redelmeier <iredelmeier@gmail.com>
2014-05-02 20:03:55 +0000

    Change hostname no longer requires sudo permission and replaces ironman/sentinel references

--- change-hostname.sh
+++ change-hostname.sh
@@ -3,11 +3,17 @@ echo "BT hostname changer"
 echo "Enter a hostname: "
 read nhost
 chost=$(hostname)
-sudo echo $nhost > /etc/hostname
+
+echo $nhost | sudo tee /etc/hostname /boot/coder_settings/hostname.txt > /dev/null
+
 sudo sed -i -e "s/$chost/$nhost/g" /etc/hosts
-sudo echo $nhost > /boot/coder_settings/hostname.txt
-sudo sed -i -e "s/$chost/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf 
+sudo sed -i -e "s/ironman/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
+
 sudo sed -i -e "s/$chost/$nhost/g" /home/coder/coder-dist/coder-base/device.json
+sudo sed -i -e "s/ironman/$nhost/g" /home/coder/coder-dist/coder-base/device.json
+
+sudo sed -i -e "s/$chost/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
+sudo sed -i -e "s/sentinel/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
 
 echo "Hostname change complete. Restarting your BlockyTalky unit. To cancel, press CTRL-C"
 echo "5..."
@@ -22,4 +28,4 @@ echo "1..."
 sleep 1
 echo "Shutting down now"
 
-sudo reboot
+#sudo reboot
