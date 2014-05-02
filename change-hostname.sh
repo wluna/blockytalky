5b41af8e32a98dd94df4c897f1d6d554084cea48
Isobel Redelmeier <iredelmeier@gmail.com>
2014-05-02 18:05:54 +0000

    Hostname change now updates wifi and coder names

--- change-hostname.sh
+++ change-hostname.sh
@@ -6,4 +6,20 @@ chost=$(hostname)
 sudo echo $nhost > /etc/hostname
 sudo sed -i -e "s/$chost/$nhost/g" /etc/hosts
 sudo echo $nhost > /boot/coder_settings/hostname.txt
-sudo sed -i -e "s/$chost/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
+sudo sed -i -e "s/$chost/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf 
+sudo sed -i -e "s/$chost/$nhost/g" /home/coder/coder-dist/coder-base/device.json
+
+echo "Hostname change complete. Restarting your BlockyTalky unit. To cancel, press CTRL-C"
+echo "5..."
+sleep 1
+echo "4..."
+sleep 1
+echo "3..."
+sleep 1
+echo "2..."
+sleep 1
+echo "1..."
+sleep 1
+echo "Shutting down now"
+
+sudo reboot
