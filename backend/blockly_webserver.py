b2afcc4bb783968a975ef4abb9169866f0161e06
Isobel Redelmeier <iredelmeier@gmail.com>
2014-05-12 15:37:44 -0400

    Added logging (and replaced the Werkzeug logger)

--- backend/blockly_webserver.py
+++ backend/blockly_webserver.py
@@ -12,16 +12,18 @@ Authorization is based on:
 
 import os
 import sys
+import logging
+import logging.handlers
 from functools import wraps
 from flask import Flask, request, Response, redirect, url_for, render_template
 from flaskext.bcrypt import Bcrypt
-from werkzeug._internal import _log
 from message import *
 import time, commands, subprocess, pika
 import jsonpickle
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
+logger = logging.getLogger('blockly_webserver')
 device_settings = {
         'password_hash': '',
         'device_name': '',
@@ -29,8 +31,6 @@ device_settings = {
         'coder_owner': '',
         'coder_color': ''
         }
-
-#_log('info', 'Server starting...')
 
 #app.debug = True
 
@@ -49,16 +49,19 @@ channel.basic_publish(exchange='', routing_key='HwCmd', body=upMsg)
 os.chdir('/home/pi/blockytalky')
 
 def load_device_settings():
-    json_file = open('/home/coder/coder-dist/coder-base/device.json', 'r')
-    device_json = jsonpickle.decode(json_file.read())
-    device_settings = {
-            'password_hash': device_json['password_hash'],
-            'device_name': device_json['device_name'],
-            'hostname': device_json['hostname'],
-            'coder_owner': device_json['coder_owner'],
-            'coder_color': device_json['coder_color']
-            }
-    json_file.close()
+    try:
+        json_file = open('/home/coder/coder-dist/coder-base/device.json', 'r')
+        device_json = jsonpickle.decode(json_file.read())
+        device_settings = {
+                'password_hash': device_json['password_hash'],
+                'device_name': device_json['device_name'],
+                'hostname': device_json['hostname'],
+                'coder_owner': device_json['coder_owner'],
+                'coder_color': device_json['coder_color']
+                }
+        json_file.close()
+    except Exception as e:
+        logger.exception('Failed to open device settings file:')
     return device_settings
 
 def requires_auth(f):
@@ -91,21 +94,22 @@ def upload():
     startTime = None
     endTime = None
     if request.method == 'POST':
+        logger.info('Uploading code')
         data = request.form.copy()
         xml_data = data['xml']
         python_data = data['python']
-        _log('info', 'Blockly code received')
         upload_code(xml_data, python_data);
+        logger.info('Blockly code uploaded')
         return 'OK'
 
 def code_to_file(code, file_name, file_label):
-    print 'Writing %s code:\n%s\n\n' % (file_name, code)
+    logger.debug('Writing %s code:\n%s\n\n' % (file_name, code))
     startTime = time.time()
     fo = open(file_name, 'wb')
     fo.write(code)
     fo.close()
     endTime = time.time()
-    print '%s file took %fs' % (file_label, endTime - startTime)
+    logger.info('%s file took %fs' % (file_label, endTime - startTime))
 
 def upload_code(xml_data, python_data):
     uploadStart = time.time()
@@ -115,10 +119,9 @@ def upload_code(xml_data, python_data):
     startTime = time.time()
     subprocess.call(['sudo pkill -9 -f user_script.py'], shell = True)
     endTime = time.time()
-    print 'Subprocess pt1 took '+ str(endTime - startTime) + ' s'
+    logger.debug('Subprocess pt1 took '+ str(endTime - startTime) + ' s')
 
-    _log('info', 'Python written!')
-    print 'Upload took '+ str(time.time() - uploadStart) + ' s'
+    logger.info('Upload took '+ str(time.time() - uploadStart) + ' s')
 
 def convert_usercode(python_code):
     # Need to use two-space tabs for consistency with Blockly conversion
@@ -135,16 +138,22 @@ def convert_usercode(python_code):
 @app.route('/stop', methods = ['GET', 'POST'])
 @requires_auth
 def stop():
-    _log('info', 'Issuing kill command')
+    logger.info('Issuing kill command')
     subprocess.call(['sudo pkill -9 -f user_script.py'], shell = True)
     #commands.getstatusoutput('python /home/pi/blockytalky/code/kill.py')
     toSend = Message('name', None, 'HwCmd', Message.createImage(motor1=0, motor2=0, motor3=0, motor4=0, pin13=0))
     toSend = Message.encode(toSend)
-    channel.basic_publish(exchange='', routing_key='HwCmd', body=toSend)
+    try:
+        channel.basic_publish(exchange='', routing_key='HwCmd', body=toSend)
+    except:
+        logger.exception('Failed to stop Blockly code:')
     return 'OK'
 
 def update_sensors(sensors):
-    assert len(sensors) == 4
+    try:
+        assert len(sensors) == 4
+    except AssertionError as e:
+        logging.exception('Update request didn\'t contain 4 sensors:')
     sensorMsg = Message('name', None, 'Sensor', 
                 Message.createImage(sensor1=sensors[0],
                 sensor2=sensors[1],
@@ -152,7 +161,10 @@ def update_sensors(sensors):
                 sensor4=sensors[3]
                 ))
     sensorMsg = Message.encode(sensorMsg)
-    channel.basic_publish(exchange='', routing_key='HwCmd', body=sensorMsg)
+    try:
+        channel.basic_publish(exchange='', routing_key='HwCmd', body=sensorMsg)
+    except:
+        logger.exception('Failed to update sensors:')
 
 @app.route('/update', methods = ['GET', 'POST'])
 @requires_auth
@@ -167,15 +179,19 @@ def update():
 @app.route('/run', methods = ['GET', 'POST'])
 @requires_auth
 def start():
-    _log('info', 'Executing code on robot')
+    logger.info('Running code on robot')
     # commands.getstatusoutput('python /home/pi/code/test.py')
     cmd = ['sudo python /home/pi/blockytalky/backend/user_script.py']
-    p = subprocess.Popen(cmd, shell = True)
+    try:
+        p = subprocess.Popen(cmd, shell = True)
+    except:
+        logger.exception('Failed to run code on robot:')
     return 'OK'
 
 @app.route('/load', methods = ['GET', 'POST'])
 @requires_auth
 def load():
+    logger.info('Loading Blockly code')
     url = url_for('static', filename='rawxml.txt', t=time.time())
     return redirect(url)
 
@@ -205,5 +221,13 @@ def authenticate():
                     {'WWW-Authenticate': 'Basic realm="Login Required"'})
 
 if __name__ == '__main__':
+    handler = logging.handlers.RotatingFileHandler(filename='/home/pi/blockytalky/logs/blockly_ws.log',
+                                                   maxBytes=5096, backupCount=3)
+    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s: %(message)s',
+                                  datefmt='%H:%M:%S %d/%m')
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
     device_settings = load_device_settings()
     app.run(host = '0.0.0.0')
