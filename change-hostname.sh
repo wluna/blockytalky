daa298636aa3423bae505678010c5747db4315c9
Joe Sanford <joe@cs.tufts.edu>
2014-05-19 18:14:38 -0400

    Change hostname script fixes

--- change-hostname.sh
+++ change-hostname.sh
@@ -7,13 +7,17 @@ chost=$(hostname)
 echo $nhost | sudo tee /etc/hostname /boot/coder_settings/hostname.txt > /dev/null
 
 sudo sed -i -e "s/$chost/$nhost/g" /etc/hosts
-sudo sed -i -e "s/ironman/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
+sudo sed -i -e "s/ironman/$nhost/g" /etc/hosts
+sudo sed -i -e "s/loki/$nhost/g" /etc/hosts
 
 sudo sed -i -e "s/$chost/$nhost/g" /home/coder/coder-dist/coder-base/device.json
 sudo sed -i -e "s/ironman/$nhost/g" /home/coder/coder-dist/coder-base/device.json
+sudo sed -i -e "s/loki/$nhost/g" /home/coder/coder-dist/coder-base/device.json
 
 sudo sed -i -e "s/$chost/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
 sudo sed -i -e "s/sentinel/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
+sudo sed -i -e "s/ironman/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
+sudo sed -i -e "s/loki/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
 hostname | sudo tee /etc/BlockyTalkyID > /dev/null
 
 echo "Hostname change complete. Restarting your BlockyTalky unit. To cancel, press CTRL-C"
