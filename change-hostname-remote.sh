7d0756b33751b5d2306c6ed6e1b4ad0470c48ed6
Isobel Redelmeier <iredelmeier@gmail.com>
2014-05-22 13:46:08 -0400

    Replace current name with a random string first to prevent issues with overlapping names; change-hostname uses change-hostname-remote

--- change-hostname-remote.sh
+++ change-hostname-remote.sh
@@ -2,19 +2,25 @@
 
 nhost=$1
 chost=$(hostname)
+rand=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
-echo $nhost | sudo tee /etc/hostname /boot/coder_settings/hostname.txt > /dev/null
+# Replace the current name with a random string in case, e.g., the new name
+# is a shorter version of the old one
+# NOTE: The last part should be changed when the units start to use GUIDs!!!
+echo $nhost | sudo tee /etc/hostname /boot/coder_settings/hostname.txt /etc/BlockyTalkyID > /dev/null
 
-sudo sed -i -e "s/$chost/$nhost/g" /etc/hosts
-sudo sed -i -e "s/ironman/$nhost/g" /etc/hosts
-sudo sed -i -e "s/loki/$nhost/g" /etc/hosts
+sudo sed -i -e "s/$chost/$rand/g" /etc/hosts
+sudo sed -i -e "s/ironman/$rand/g" /etc/hosts
+sudo sed -i -e "s/loki/$rand/g" /etc/hosts
+sudo sed -i -e "s/$rand/$nhost/g" /etc/hosts
 
-sudo sed -i -e "s/$chost/$nhost/g" /home/coder/coder-dist/coder-base/device.json
-sudo sed -i -e "s/ironman/$nhost/g" /home/coder/coder-dist/coder-base/device.json
-sudo sed -i -e "s/loki/$nhost/g" /home/coder/coder-dist/coder-base/device.json
+sudo sed -i -e "s/$chost/$rand/g" /home/coder/coder-dist/coder-base/device.json
+sudo sed -i -e "s/ironman/$rand/g" /home/coder/coder-dist/coder-base/device.json
+sudo sed -i -e "s/loki/$rand/g" /home/coder/coder-dist/coder-base/device.json
+sudo sed -i -e "s/$rand/$nhost/g" /home/coder/coder-dist/coder-base/device.json
 
-sudo sed -i -e "s/$chost/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
-sudo sed -i -e "s/sentinel/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
-sudo sed -i -e "s/ironman/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
-sudo sed -i -e "s/loki/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
-hostname | sudo tee /etc/BlockyTalkyID > /dev/null
+sudo sed -i -e "s/$chost/$rand/g" /etc/wpa_supplicant/wpa_supplicant.conf
+sudo sed -i -e "s/sentinel/$rand/g" /etc/wpa_supplicant/wpa_supplicant.conf
+sudo sed -i -e "s/ironman/$rand/g" /etc/wpa_supplicant/wpa_supplicant.conf
+sudo sed -i -e "s/loki/$rand/g" /etc/wpa_supplicant/wpa_supplicant.conf
+sudo sed -i -e "s/$rand/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
--- change-hostname.sh
+++ change-hostname.sh
@@ -2,23 +2,8 @@
 echo "BT hostname changer"
 echo "Enter a hostname: "
 read nhost
-chost=$(hostname)
 
-echo $nhost | sudo tee /etc/hostname /boot/coder_settings/hostname.txt > /dev/null
-
-sudo sed -i -e "s/$chost/$nhost/g" /etc/hosts
-sudo sed -i -e "s/ironman/$nhost/g" /etc/hosts
-sudo sed -i -e "s/loki/$nhost/g" /etc/hosts
-
-sudo sed -i -e "s/$chost/$nhost/g" /home/coder/coder-dist/coder-base/device.json
-sudo sed -i -e "s/ironman/$nhost/g" /home/coder/coder-dist/coder-base/device.json
-sudo sed -i -e "s/loki/$nhost/g" /home/coder/coder-dist/coder-base/device.json
-
-sudo sed -i -e "s/$chost/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
-sudo sed -i -e "s/sentinel/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
-sudo sed -i -e "s/ironman/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
-sudo sed -i -e "s/loki/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
-hostname | sudo tee /etc/BlockyTalkyID > /dev/null
+sh /home/pi/blockytalky/change-hostname-remote.sh $nhost
 
 echo "Hostname change complete. Restarting your BlockyTalky unit. To cancel, press CTRL-C"
 echo "5..."
