10d2a5fe5ec722e3064816407ecf7d86fe6830de
Joe <josephsanford@gmail.com>
2014-06-16 16:25:21 +0000

    Changed update script to pull new version, force pull to ignore conflicts

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 cd /home/pi/blockytalky
-# git pull
-[ "`git log --pretty=%H ...refs/heads/master^ | head -n 1`" = "`git ls-remote origin -h refs/heads/master |cut -f1`" ] && echo "Up to date" || git pull
+git fetch --all
+git reset --hard origin/master
