2d64ebcfcd11aed089fd798b2fb23db75751d2ce
Joe Sanford <joe@cs.tufts.edu>
2014-06-18 08:07:58 -0400

    Revert ultrasonic sensor type in HD

--- backend/hardware_daemon.py
+++ backend/hardware_daemon.py
@@ -184,7 +184,7 @@ class HardwareDaemon(object):
                         if value == "touch":
                             newType = TYPE_SENSOR_TOUCH
                         if value == "ultra":
-                            newType = TYPE_SENSOR_I2C
+                            newType = TYPE_SENSOR_ULTRASONIC_CONT
                         if value == "sound":
                             newType = TYPE_SENSOR_SOUND
                         if value == "light_on":
