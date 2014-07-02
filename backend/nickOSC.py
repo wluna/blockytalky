ef69eb039612d9ae1f520540ce12371f8ae43eaa
Nick Benson <nickjbenson@gmail.com>
2014-07-02 13:00:03 -0400

    Debugging connection error + attempt to make connections more robust

--- backend/nickOSC.py
+++ backend/nickOSC.py
@@ -15,10 +15,6 @@ def simple_play(notes):
 	message.setAddress("lpc/maestro/play")
 	# copy note content into message
 	for i in range(len(notes)):
-		print "currently on " + str(notes[i])
-		print notes
-		print notes[i]
-		print notes[i][0]
 		message.append(float(notes[i][0]))
 		message.append(int(notes[i][1]))
 	# pad message with empty notes
@@ -26,5 +22,10 @@ def simple_play(notes):
 	for i in range(64 - len(notes)):
 		message.append(-1)
 		message.append(0.)
-	client.send(message)
+	try:
+		client.send(message)
+	except OSC.OSCClientError as e:
+		print "Error while sending: " + str(e)
+		print "Trying to reconnect."
+		client.connect( (note_destination_hostname, note_destination_port) )
 	print "message sent to " + note_destination_hostname + " from simple_play"
