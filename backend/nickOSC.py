7262641a72feacf6fe862203fea68b89772045ed
Nick Benson <nickjbenson@gmail.com>
2014-07-02 12:43:07 -0400

    Debug printing

--- backend/nickOSC.py
+++ backend/nickOSC.py
@@ -14,6 +14,7 @@ def simple_play(notes):
 	message = OSC.OSCMessage()
 	# copy note content into message
 	for i in range(len(notes)):
+		print str(notes[i][0])
 		message.append(float(notes[i][0]))
 		message.append(int(notes[i][1]))
 	# pad message with empty notes
