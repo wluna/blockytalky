b4ae8349fe216527004baec47addc215517b008d
Nick Benson <nickjbenson@gmail.com>
2014-07-23 15:21:55 -0400

    Debugging drum generator

--- blockly/static/generators/python/music.js
+++ blockly/static/generators/python/music.js
@@ -1001,6 +1001,8 @@ Blockly.Python.music_drum_sequence = function() {
 		for (var j = 0; j < 16; j++) {
 			var one_or_zero_str = this.getTitleValue(intToDrumkitNoteString(i) + j)
 						== true ? "1" : "0";
+			console.log((intToDrumkitNoteString(i) + j));
+			console.log(this.getTitleValue(intToDrumkitNoteString(i) + j));
 			sequence_data = sequence_data.concat(one_or_zero_str);
 			if (j < 15)
 				sequence_data = sequence_data.concat(", ");
