34ecb5d003064aeb93b5bed3451ffb422a4bfdd6
Nick Benson <nickjbenson@gmail.com>
2014-07-23 15:29:40 -0400

    Attempting to fix drum machine checkbox checking

--- blockly/static/generators/python/music.js
+++ blockly/static/generators/python/music.js
@@ -999,10 +999,8 @@ Blockly.Python.music_drum_sequence = function() {
 	for (var i = 1; i < 8; i++) {
 		sequence_data = sequence_data.concat("[");
 		for (var j = 0; j < 16; j++) {
-			var one_or_zero_str = this.getTitleValue(intToDrumkitNoteString(i) + j)
-						== true ? "1" : "0";
-			console.log((intToDrumkitNoteString(i) + j));
-			console.log(this.getTitleValue(intToDrumkitNoteString(i) + j));
+			var one_or_zero_str = this.getTitleValue(intToDrumkitNoteString(i+1) + j)
+						== "TRUE" ? "1" : "0";
 			sequence_data = sequence_data.concat(one_or_zero_str);
 			if (j < 15)
 				sequence_data = sequence_data.concat(", ");
