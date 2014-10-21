e1ed86488694bda8370586b554ac70dc6f970744
mpahrens <Matt.p.ahrens@gmail.com>
2014-10-21 11:57:19 -0400

    Salvaged old change hostname from coder image

--- change-hostname.sh
+++ change-hostname.sh
@@ -2,20 +2,8 @@
 echo "BT hostname changer"
 echo "Enter a hostname: "
 read nhost
-
-sh /home/pi/blockytalky/change-hostname-remote.sh $nhost
-
-echo "Hostname change complete. Restarting your BlockyTalky unit. To cancel, press CTRL-C"
-echo "5..."
-sleep 1
-echo "4..."
-sleep 1
-echo "3..."
-sleep 1
-echo "2..."
-sleep 1
-echo "1..."
-sleep 1
-echo "Shutting down now"
-
-sudo reboot
+chost=$(hostname)
+sudo echo $nhost > /etc/hostname
+sudo sed -i -e "s/$chost/$nhost/g" /etc/hosts
+sudo echo $nhost > /boot/coder_settings/hostname.txt
+sudo sed -i -e "s/$chost/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
