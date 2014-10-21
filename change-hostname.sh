1d91057cdae153feed559916bc657947ef96bd69
mpahrens <Matt.p.ahrens@gmail.com>
2014-10-21 12:33:05 -0400

    Forgot ben helm's changes to make it work with coder and BT

--- change-hostname.sh
+++ change-hostname.sh
@@ -3,7 +3,30 @@ echo "BT hostname changer"
 echo "Enter a hostname: "
 read nhost
 chost=$(hostname)
-sudo echo $nhost > /etc/hostname
+
+echo $nhost | sudo tee /etc/hostname /boot/coder_settings/hostname.txt > /dev/null
+
 sudo sed -i -e "s/$chost/$nhost/g" /etc/hosts
-sudo echo $nhost > /boot/coder_settings/hostname.txt
+sudo sed -i -e "s/ironman/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
+
+sudo sed -i -e "s/$chost/$nhost/g" /home/coder/coder-dist/coder-base/device.json
+sudo sed -i -e "s/ironman/$nhost/g" /home/coder/coder-dist/coder-base/device.json
+
 sudo sed -i -e "s/$chost/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
+sudo sed -i -e "s/sentinel/$nhost/g" /etc/wpa_supplicant/wpa_supplicant.conf
+sudo hostname | tee /etc/BlockyTalkyID > /dev/null
+
+echo "Hostname change complete. Restarting your BlockyTalky unit. To cancel, press CTRL-C"
+echo "5..."
+sleep 1
+echo "4..."
+sleep 1
+echo "3..."
+sleep 1
+echo "2..."
+sleep 1
+echo "1..."
+sleep 1
+echo "Shutting down now"
+
+sudo reboot
